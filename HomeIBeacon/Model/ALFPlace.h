7d8040d868803eb95373204e05aeee4a683cdcb3
allfake <allfake24@gmail.com>
2014-05-17 16:16:14 +0700

    first commit

---
+++ HomeIBeacon/Model/ALFPlace.h
@@ -0,0 +1,13 @@
+//
+//  ALFPlace.h
+//  HomeIBeacon
+//
+//  Created by allfake on 5/17/14.
+//  Copyright (c) 2014 allfake. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ALFPlace : NSObject
+
+@end
---
+++ HomeIBeacon/Model/ALFPlace.m
@@ -0,0 +1,13 @@
+//
+//  ALFPlace.m
+//  HomeIBeacon
+//
+//  Created by allfake on 5/17/14.
+//  Copyright (c) 2014 allfake. All rights reserved.
+//
+
+#import "ALFPlace.h"
+
+@implementation ALFPlace
+
+@end
